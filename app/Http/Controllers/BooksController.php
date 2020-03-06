281ad94432c6c0cbd97763143a4eaed80fd62074
fpyc <nuchnum65@gmail.com>
2020-03-06 20:36:45 +0700

    edit & delete product by 60003263012

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Books; //อย่าลืม use model เข้ามาใช้งาน
 use App\Http\Requests\StoreBooksRequest;
 use Image;//เรียกใช้ library จัดการรูปภาพเข้ามาใช้งาน
+use File;
 class BooksController extends Controller
 {
     /**
@@ -47,7 +48,7 @@ class BooksController extends Controller
             Image::make(public_path().'/images/'.$filename)->resize(50, 50)->save(public_path(). '/images/resize/'. $filename);
             $book->image = $filename;
         }else{
-            $book->image = 'nopic.jpg';
+            $book->image = 'nopic.png';
         }
         $book->save();
         return redirect()->action('BooksController@index');
@@ -72,7 +73,8 @@ class BooksController extends Controller
      */
     public function edit($id)
     {
-        //
+        $book = Books::findOrFail($id);
+        return view('books.edit', ['book' => $book]);
     }
 
     /**
@@ -82,9 +84,29 @@ class BooksController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(StoreBooksRequest $request, $id)
     {
         //
+        $book = Books::find($id);
+        $book->title = $request->title;
+        $book->price = $request->price;
+        $book->typebooks_id = $request->typebooks_id;
+
+        if($request->hasFile('image')){
+            // delete old file before update
+            if($book->image != 'nopic.png'){
+                File::delete(public_path(). "\\images\\".$book->image);
+                File::delete(public_path()."\\images\\resize\\".$book->image);
+            }
+
+            $filename = str_random(10).'.'.$request->file('image')->getClientOriginalExtension();
+            $request->file('image')->move(public_path().'/images/'.$filename);
+            Image::make(public_path().'/images/'.$filename)->resize(50, 50)->save(public_path().'/images/resize/'.$filename);
+            $book->image = $filename;
+        }
+
+        $book->save();
+        return redirect()->action('BooksController@index');
     }
 
     /**
@@ -96,5 +118,14 @@ class BooksController extends Controller
     public function destroy($id)
     {
         //
+        $book = Books::find($id);
+        if($book->image != 'nopic.png'){
+            File::delete(public_path(). "\\images\\".$book->image);
+            File::delete(public_path()."\\images\\resize\\".$book->image);
+        }
+
+        $book->delete();
+        return redirect()->action('BooksController@index');
+
     }
 }
--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-lg-10 col-lg-offset-1">
             <?= link_to('books/create', $title='เพิ่มข้อมูล', ['class' => 'btn btn-primary'], $secure = null); ?>
-            <div class="card">
+            <div class="card mt-3">
                 <div class="card-header h3">
                     แสดงข้อมูลหนังสือ จำนวนทั้งหมด [{{ $books->total() }} เล่ม]
                 </div>
@@ -17,6 +17,8 @@
                             <th>ราคา</th>
                             <th>หมวดหนังสือ</th>
                             <th>รูปภาพ</th>
+                            <th>แก้ไข</th>
+                            <th>ลบ</th>
                         </tr>
                         @foreach($books as $book)
                         <tr>
@@ -25,6 +27,12 @@
                             <td>{{ number_format($book->price,2) }}</td>
                             <td>{{ $book->typebooks->name }}</td>
                         <td><a href="{{ asset('images/'.$book->image) }}"><img src="{{ asset('images/resize/'.$book->image) }}" style="width:100px"></a></td>
+                        <td><a href="{{ url('/books/'.$book->id.'/edit') }}">แก้ไข</a></td>
+                        <td>
+                            <?= Form::open(array('url' => 'books/'.$book->id, 'method' => 'delete','onsubmit' => 'return confirm("แน่ใจว่าต้องการลบข้อมูล?");')) ?>
+                            <button type="submit" class="btn btn-danger">ลบ</button>
+                            {!! Form::close() !!}
+                        </td>
                         </tr>
                         @endforeach
                     </table>
